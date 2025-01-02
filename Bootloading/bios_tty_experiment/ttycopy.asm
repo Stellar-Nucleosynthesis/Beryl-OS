8a16e51d7ee6fe3d004d629a99f698850791bcae
Nazar0k356 <stepanenkonazar356@gmail.com>
2025-01-02 20:53:09 +0200

    Project structure changed
    All non-ASCII characters deleted. Documentation now resides in a separate folder.

--- Bootloading/bios_tty_experiment/ttycopy.asm
+++ Bootloading/bios_tty_experiment/ttycopy.asm
@@ -1,23 +1,23 @@
 [BITS 16]
-[ORG 0x7C00]  ; Завантажувач починається з цієї адреси в пам'яті
+[ORG 0x7C00]  ; The program starts here in memory
 
 start:
 wait_key:
-    xor ah, ah           ; Очікування натискання клавіші
-    int 0x16             ; Виклик BIOS для отримання коду натиснутої клавіші
-    cmp al, 0x1B         ; Перевірка, чи була натиснута клавіша Esc (код 0x1B)
-    je exit              ; Вихід, якщо Esc
-    cmp al, 0x20         ; Перевіряємо, чи це символ для друку
-    jb wait_key          ; Пропустити все, що не є символом
-    cmp al, 0x7F         ; Пропустити все, що за межами ASCII
-    ja wait_key
-    mov ah, 0x0E
-    int 0x10             ; Вивести символ
-    jmp wait_key         ; Повторити очікування вводу
+    xor ah, ah           ; Loading INT num
+    int 0x16             ; Waiting for a key being pressed
+    cmp al, 0x1B         ; Checking for "esc"
+    je exit              ; If yes, exit
+    cmp al, 0x20         ; Checking for a printable char
+    jb wait_key          ; If not, skip
+    cmp al, 0x7F         ; Checking for an ASCII char
+    ja wait_key          ; If not, skip
+    mov ah, 0x0E         ; Loading INT num
+    int 0x10             ; Outputting the symbol
+    jmp wait_key         ; To the beginning we go
 
 exit:
-    cli                  ; Заборонити переривання
-    hlt                  ; Завершити виконання
+    cli                  ; Prohibiting INTs
+    hlt                  ; That's it
 
-times 510-($-$$) db 0    ; Заповнити нулями до 510 байтів
-dw 0xAA55               ; Завантажувальний підпис (boot signature)
+times 510-($-$$) db 0    ; Filling the rest with 0's
+dw 0xAA55               ; Boot signature
